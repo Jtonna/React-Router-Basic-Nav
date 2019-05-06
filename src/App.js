4f4add006f5ab593d099ab69d21efca780a2d1c3
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-05-06 14:46:36 -0700

    Update App.js
    Added routing paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path ='/' component={Home}></Route>
+    {/* exact path ='/' --- is used so 'Home" doesnt render along with About, or wwhatever else is clicked. this is only needed on the first react-router obj */}
+    <Route path='/About' component={About}></Route>
+    <Route path='/Contact' component={Contact}></Route>
+    <Route path='/Navigation' component={Navigation}></Route>
   </div>
 );
 
